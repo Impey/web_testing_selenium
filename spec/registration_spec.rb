abb2ba20a9601c491aabf28b26854265e761c275
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2019-02-05 13:58:51 +0000

    added faker gem in order to generate random data

--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -3,13 +3,21 @@ describe 'testing the demoq automation form' do
    @driver = QatoolsForm.new
    @driver.visit_practise_form
    @url = 'https://www.toolsqa.com/automation-practice-form'
+   @name =  Faker::HarryPotter.character
+   @place = Faker::HarryPotter.location
   end
   context 'testing the postive paths for the form' do
   
   it 'should land on the registraction page' do
-   expect(driver.current_url).to eq @url
+   expect(@driver.current_url).to eq @url
   end
+
+  it 'should accpt a first name' do
+    @driver.input_firstname_field(@name)
+    expect(@driver.input_firstname_field_value).to eq @name
+   end
 
 
   end
 end
+
